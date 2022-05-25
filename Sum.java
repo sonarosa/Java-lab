ca6a80c7ab72a71b5f106fa632cbb37fd4ae9355
sonarosa <95519996+sonarosa@users.noreply.github.com>
2022-05-25 20:00:05 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main
+{
+public static void main(String[] args) 
+{ 
+	Scanner sc = new Scanner(System.in); 
+	int num1=6;
+	int num2=7;
+    int sum=num1+num2;
+    System.out.println("Number 1 : " + num1); 
+    System.out.println("Number 2 : " + num2); 
+	System.out.println("Sum of the two numbers : " + sum); 
+} 
+}
